ac557598dbee9fa8201377026d7131159e26f26d
Sunny kumar <120244832+sunny8750@users.noreply.github.com>
2023-08-07 15:36:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ function delayAndResolve() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("Hello, world!");
+        }, 1000); // 1 second
+      });
+    }
+
+    const outputElement = document.getElementById("output");
+
+    delayAndResolve().then((message) => {
+      outputElement.textContent = message;
+    });
